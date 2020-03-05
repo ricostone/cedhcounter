66914e6786eeb7e96ec0eb298cfbd3c119d198b2
e.vercherat <e.vercherat@til-technologies.fr>
2020-03-05 12:02:49 +0100

    First commit setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className={"app-title"}>cEDH Counter</div>
+      <div className={"app-subtitle"}>Welcome to</div>
+      <button className={"btn btn-primary"}>New Game</button>
+    </div>
+  );
+}
+
+export default App;
